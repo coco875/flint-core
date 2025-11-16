13751f406b1cacb82b4a2a5105903ab2f416c195
JunkyDeveloper <junkydeveloper@gmail.com>
2025-11-16 17:39:05 +0100

    added indexing to it, to have quicker loading if used only tagged tests

--- src/test_spec.rs
+++ src/test_spec.rs
@@ -1,5 +1,10 @@
 use serde::{Deserialize, Serialize};
-use std::path::PathBuf;
+use serde_json::to_string_pretty;
+use std::collections::{BTreeMap, HashMap};
+use std::fs;
+use std::fs::{File, OpenOptions};
+use std::io::{BufReader, Write};
+use std::path::{Path, PathBuf};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -97,6 +102,79 @@ impl TestSpec {
     pub const MAX_WIDTH: i32 = 15;
     pub const MAX_HEIGHT: i32 = 384;
     pub const MAX_DEPTH: i32 = 15;
+    pub const INDEX_NAME: &'static str = "index.json";
+    pub const DEFAULT_TAG: &'static str = "default";
+    const TEST_PATH: &'static str = "./test";
+
+    pub fn generate_index(path: &Path) -> anyhow::Result<()> {
+        if !path.is_dir() {
+            eprintln!("'{}' is no directory", path.display());
+            std::process::exit(1);
+        }
+        let index = get_all_test_files(path);
+        let mut new_index: BTreeMap<String, Vec<String>> = BTreeMap::new();
+        for i in index {
+            let file = File::open(i.clone())?;
+            let reader = BufReader::new(file);
+            let test: TestSpec =
+                serde_json::from_reader(reader).expect("broken test file, isn't json!");
+            for tag in &test.tags {
+                if let Some(vec) = new_index.get_mut(tag) {
+                    vec.push(i.clone())
+                } else {
+                    new_index.insert(tag.clone(), vec![i.clone()]);
+                }
+            }
+            if test.tags.is_empty() {
+                if let Some(vec) = new_index.get_mut(TestSpec::DEFAULT_TAG) {
+                    vec.push(i.clone())
+                } else {
+                    new_index.insert(TestSpec::DEFAULT_TAG.into(), vec![i.clone()]);
+                }
+            }
+        }
+        let index_string = to_string_pretty(&new_index).expect("BTree for index is broken");
+        let mut file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .open(TestSpec::INDEX_NAME)?;
+        file.write_all(index_string.as_bytes())?;
+        Ok(())
+    }
+
+    pub fn load_tagged_tests_paths(scope: &[String]) -> anyhow::Result<Vec<String>> {
+        if !Path::new("index.json").exists() {
+            println!("Index does not exists, so need to build the index first");
+            TestSpec::generate_index(&Path::new(TestSpec::TEST_PATH))?;
+        }
+        let mut test_paths = vec![];
+        let file = File::open("index.json")?;
+        let reader = BufReader::new(file);
+        let map: HashMap<String, Vec<String>> =
+            serde_json::from_reader(reader).expect("broken index, isn't json!");
+        for (k, v) in &map {
+            if scope.contains(&k) {
+                for path in v {
+                    println!("added test to current run: '{}' ", path);
+                    test_paths.push(path.clone());
+                }
+            }
+        }
+        Ok(test_paths)
+    }
+    
+    pub fn get_all_tests_paths() -> anyhow::Result<Vec<String>> {
+        let test_paths: Vec<String>;
+        if !Path::new(TestSpec::INDEX_NAME).exists() {
+            println!("Index does not exists, so need to build the index first");
+            test_paths = get_all_test_files(Path::new(TestSpec::TEST_PATH));
+        } else {
+            let file = File::open(TestSpec::INDEX_NAME)?;
+            let reader = BufReader::new(file);
+            test_paths = serde_json::from_reader(reader).expect("broken index, isn't json!");
+        }
+        Ok(test_paths)
+    }
 
     pub fn from_file(path: &PathBuf) -> anyhow::Result<Self> {
         let content = std::fs::read_to_string(path)?;
@@ -237,4 +315,25 @@ impl TestSpec {
         }
         Ok(())
     }
+}
+
+fn get_all_test_files(start_dir: &Path) -> Vec<String> {
+    let mut dirs = vec![start_dir.to_path_buf()];
+    let mut index: Vec<String> = Vec::new();
+
+    while let Some(dir) = dirs.pop() {
+        if let Ok(entries) = fs::read_dir(&dir) {
+            for entry in entries.flatten() {
+                let path = entry.path();
+                if path.is_dir() {
+                    dirs.push(path);
+                } else if let Some(ext) = path.extension() {
+                    if ext == "json" {
+                        index.push(path.to_str().unwrap().to_string());
+                    }
+                }
+            }
+        }
+    }
+    index
 }
